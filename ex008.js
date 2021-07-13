40d5fc82d77a3a19530986418e30857f4069c339
LetíciaS <69993315+LeticiaTr@users.noreply.github.com>
2021-07-13 19:36:18 -0300

    Testando DOM

---
+++ ex008.js
@@ -0,0 +1,4 @@
+var vel = 12
+console.log(`A velocidade de seu carri é ${vel}Km/h`)
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
