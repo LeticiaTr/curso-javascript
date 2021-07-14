9bfd274fccf93a384138afa536da12a867f82e11
LetíciaS <69993315+LeticiaTr@users.noreply.github.com>
2021-07-13 21:53:23 -0300

    Desafio concluído

--- ex008.js
+++ ex008.js
@@ -1,4 +1,5 @@
-var vel = 12
-console.log(`A velocidade de seu carri é ${vel}Km/h`)
-console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+var vel = 60.5
+console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if (vel >60){ //Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)}
 console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ ex009.js
@@ -0,0 +1,6 @@
+var pais = "Estados Unidos"
+console.log (`Vivendo no ${pais}`)
+if (pais == "Brasil") {
+    console.log("Você é brasileiro")
+}else
+    console.log('Você é estrangeiro')
